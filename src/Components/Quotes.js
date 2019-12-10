d1bedadb8218c43637c9c0050345233d482191e9
Benjamin Dryden <ben.dryden@gmail.com>
2019-12-10 01:08:56 +0100

    axios api call good, needs button

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -6,29 +6,32 @@ export class Quotes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quoteText: '',
+            quoteText: ''
         }
-    }
+    } /*Here we set our state which will be updated next when the component loads or mounts*/
 
     componentDidMount() {
         axios.get('https://api.kanye.rest')
             .then(response => {
                 this.setState({ quoteText: response.data["quote"] });
-                console.log('HI', response)
             })
             .catch(error => {
                 console.log(error);
             });
-    }
+    }        /*Here we call our 3rd party api for quotes, their formatting needs to be understood, can use postman to understand and
+    manage. The returned quote then becomes the current state. We also use a callback to load any errors in the console. */
 
 render() {
     const { quoteText } = this.state;
     return (
         <div>
             <p>"{quoteText}"</p>
+            <button onClick={this.getClicked}
+                    type='button'>More Wisdom</button>
         </div>
     );
-}
+}   /*Here we actually show the result in jsx form as a variable and finally export it as a component to the parent
+where it can be displayed*/
 }
 
 
