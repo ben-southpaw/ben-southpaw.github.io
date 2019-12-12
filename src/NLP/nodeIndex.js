8880c9a685e75c67484b15e66bf59f33c59c25ef
Benjamin Dryden <ben.dryden@gmail.com>
2019-12-12 15:18:37 +0100

    removed file with sensitive data

--- src/NLP/nodeIndex.js
+++ src/NLP/nodeIndex.js
@@ -1,10 +1,10 @@
 
 
 /*Here we will call any external api from which we want to get data from to analyse in our node backend.
-Request is an ideal library for http requests. We have a simple callback function in which
- we add our api link, parse the response body and cater for errors.*/
+Axios is an ideal library for http requests. We have a async ES6 function in which means we can allow for the response
+time of the external api. We add our api link, parse the response body and cater for errors.*/
 const axios = require('axios');
-const language = require('@google-cloud/language');
+const language = require('@google-cloud/language');//Accessing the google apis
 
 const getQuotes = async () => {
     try {
@@ -14,28 +14,9 @@ const getQuotes = async () => {
     }
 };
 
-/*const showQuotes = async () => {
-    const returnedQuote = await getQuotes();
-    if (returnedQuote.data.quote) {
-        const arrayOfSingleQuote = returnedQuote.data.quote;
-        console.log(arrayOfSingleQuote);
-        return arrayOfSingleQuote
-    }.then(arrayOfSingleQuote => {
-
-    })
-};
-
-
-const text = showQuotes(getQuotes(), null);
-
-
-//console.log(text);*/
-
-
 const analyseQuote = async () => {
     const returnedQuote = await getQuotes();
     const text = returnedQuote.data.quote;
-    console.log(text);
     const document = {
         content: text,
         type: 'PLAIN_TEXT',
@@ -50,13 +31,12 @@ const analyseQuote = async () => {
             })
             .catch(err => {
                 console.error('ERROR:', err);
-            });
+            }); /*In this function we call getQuotes and get either the response or an error. From pass the response as
+            a formatted JSON object to the analyse sentiment library and return the results of each quote or catch an error*/
 
 
 
 };
 
 
-
-//showQuotes();
 analyseQuote();
