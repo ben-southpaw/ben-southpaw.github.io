d0192a4ae544a58e2f16e9d0dbe77f56e48e3d9f
Benjamin Dryden <benjamin@adventureclub.io>
2020-04-20 19:01:18 +0200

    Updates

---
+++ precache-manifest.4775b41687cce87285330ae67e3c8c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b053cce963f1645d91b28d89255fe786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a41ed871fde8303222",
+    "url": "/static/css/main.e135d09e.chunk.css"
+  },
+  {
+    "revision": "b4d5e1c6891593bc5a17",
+    "url": "/static/js/2.f113bd04.chunk.js"
+  },
+  {
+    "revision": "ec01d0b6e20f67eda677fcae2fe24266",
+    "url": "/static/js/2.f113bd04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40a41ed871fde8303222",
+    "url": "/static/js/main.f15b6624.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
